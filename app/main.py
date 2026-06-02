9dfadedf3612aac53e0b121c0a20b40a638018c0
Amani KRID <amani.krid@ensi-uma.tn>
2026-06-02 13:28:32 +0200

    Refactor FastAPI app initialization and imports

--- app/main.py
+++ app/main.py
@@ -1,22 +1,26 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
- 
+
 from database.database import connect_db, close_db
-from api.routes import  images_router
- 
+from api.routes import images_router
+
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await connect_db()
     yield
     await close_db()
- 
- 
+
+
 app = FastAPI(title="Serveur d'enregistrement d'images", version="1.0.0", lifespan=lifespan)
- 
+
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["POST"], allow_headers=["*"])
- 
+
 app.include_router(images_router)
- 
- 
+
+
 @app.get("/")
 async def root():
     return {"status": "ok", "docs": "/docs"}
